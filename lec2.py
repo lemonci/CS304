7f6d608429d8e8c0995195306c1f3ea52fcf0849
lemonci <hongyanchangfei@gmail.com>
2022-01-22 19:55:33 -0500

    lec 2 testing

--- lec2.py
+++ lec2.py
@@ -40,7 +40,6 @@ class CreditCard:
         """Charge given price to the card, assuming sufficient credit limit.
         
         Return True if charge was processed; False if charge was denied."""
-        
         if price + self._balance > self._limit:    # if charge would exceed limit.
             return False                           # cannot accept charge
         else:
@@ -50,3 +49,28 @@ class CreditCard:
     def make_payment(self, amount):
         """Process customer payment that reduces balance."""
         self._balance -= amount
+"""Testing the class"""
+if __name__ == '__main__':
+    wallet = []
+    wallet.append(CreditCard('John Bowman', 'California Saving',
+                             '5391 0375 9387 5309', 2500))
+    wallet.append(CreditCard('John Bowman', 'California Federal',
+                             '3485 0399 3395 1954', 3500))
+    wallet.append(CreditCard('John Bowman', 'California Finance',
+                             '5391 0375 9387 5309', 5000))
+    
+    for val in range(1, 17):
+        wallet[0].charge(val)
+        wallet[1].charge(2*val)
+        wallet[2].charge(3*val)
+        
+    for c in range(3):
+        print('Customer =', wallet[c].get_customer())
+        print('Bank =', wallet[c].get_bank())
+        print('Account =', wallet[c].get_account())
+        print('Limit =', wallet[c].get_limit())
+        print('Balance =', wallet[c].get_balance())
+        while wallet[c].get_balance() > 100:
+            wallet[c].make_payment(100)
+            print('New balance =', wallet[c].get_balance())
+        print()
