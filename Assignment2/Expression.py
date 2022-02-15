f107efa1316a21d605467f806d05fadecb4f1ae3
lemonci <hongyanchangfei@gmail.com>
2022-02-14 21:10:44 -0500

    in_to_post change

--- Assignment2/Expression.py
+++ Assignment2/Expression.py
@@ -38,38 +38,36 @@ class Expression():
             if self.is_empty():
                 raise Empty('Stack is empty')
             return self._data.pop()         # remove last item from list
+        
+#        def full_stack(self):
+#            return self._data
 
     def in_to_post():
         print('Input an infix expression, leave spaces around operands, operators and parenthesis: ')
         _in = input().split()
         output = '' 
-        S = _ArrayStack()
-        S.push('(')         # Push “(“onto Stack
+        S = Expression._ArrayStack()
+        S.push('(')         # Push “(“ onto Stack
         _in.append(')')     # and add “)” to the end of input.
         for elem in _in:    # Scan the infix expression from left to right
             if elem == '(':     # If a left parenthesis is encountered,
-                S.push(elem)    # push it onto Stack.
-            elif elem == ')':   # If a right parenthesis is encountered ,then:
-                temp = S.pop()  # pop the stack and output it until a ‘(‘ is encountered
-                while temp != '(':  # and discard both the parenthesis.
-                    output += temp
-                    temp = S.pop()
-            elif elem not in '+-*/':    # If an operand is encountered, 
+                S.push(elem)    # push it onto Stack.)
+            elif elem == ')':   # If a right parenthesis is encountered ,then:  
+                while S.top() != '(':
+                    output += S.pop()   # pop the stack and add it to output until a ‘(‘ is encountered
+                S.pop()      # remove the left parenthesis
+            elif elem not in '+-*/':    # If an operand is encountered,
                 output += elem          # add it to output
-            else:   #If an operator is encountered
-                temp = S.pop()          # Repeatedly pop from Stack 
-                if elem in '+-':        # and add to output each operator 
-                    while temp != '(':  # (on the top of Stack) which has the same precedence as or 
-                        output += temp  # higher precedence than operator.
-                        temp = S.pop()
-                    S.push(temp)
+            else:   #If an operator is encountered         
+                if elem in '+-':        # and add to output each operator
+                    while S.top() != '(':  # (on the top of Stack) which has the same precedence as or higher precedence than operator.)
+                        output += S.pop()   # Repeatedly pop from Stack
                     S.push(elem)        # Add operator to Stack
                 else:
-                    while temp not in '(/*':
-                        output += temp
-                        temp = S.pop()
-                    S.push(temp)
+                    while S.top() not in '(+-':
+                        output += S.pop()
                     S.push(elem)        # Add operator to Stack
         return output
                 
+    def post_to_in():
         
