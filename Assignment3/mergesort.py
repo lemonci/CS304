692bc47a7172d9d055e665dbbe2b89f7454eba49
lemonci <hongyanchangfei@gmail.com>
2022-03-09 22:07:48 -0500

    clean up

--- Assignment3/mergesort.py
+++ Assignment3/mergesort.py
@@ -1,3 +1,8 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 class _DoublyLinkedBase:
     """A base class providing a doubly linked list representation."""
     
@@ -46,37 +51,6 @@ class _DoublyLinkedBase:
         node._prev = node._next = node._element = None   # depreciate node
         return element                                   # return deleted element
  
-
-"""
-# A Doubly Linked List Node
-class Node:
-    def __init__(self, data, next=None, prev=None):
-        self.data = data
-        self.next = next
-        self.prev = prev
- 
-# Utility function to push a node at the beginning of the doubly linked list
-def push(head, key):
- 
-    node = Node(key, head)
- 
-    # change `prev` of the existing head node to point to the new node
-    if head:
-        head.prev = node
- 
-    # return new head node
-    return node
- 
- 
-# Helper function to print nodes of a doubly linked list
-def printDDL(head):
- 
-    while head:
-        print(head.data, end=' ⇔ ')
-        head = head.next
-    print('None')
-""" 
- 
 # Function to split nodes of the given doubly linked list into
 # two halves using the fast/slow pointer strategy
 def split(head):
@@ -99,10 +73,10 @@ def split(head):
 def d_merge(a, b):
  
     # base cases
-    if a is None:
+    if a or a._element is None:
         return b
  
-    if b is None:
+    if b or b._element is None:
         return a
  
     # pick either `a` or `b`, and recur
@@ -139,34 +113,9 @@ def d_mergesort(head):
     # merge the two sorted lists
     head = d_merge(a, b)
     return head
- 
- 
-if __name__ == '__main__':
- 
-    keys = [6, 4, 8, 7, 9, 2, 1]
-    
-    d = _DoublyLinkedBase()
-    
-    for key in keys:
-        d._insert_between(key, d._header, d._header._next)
-        
-    dn = d._header
-    while dn._next != None:
-        print(dn._element)
-        dn = dn._next
-        
- 
-    head = d_mergesort(d._header._next)
-
-    dn = d._header
-    while dn._next != None:
-        print(dn._element)
-        dn = dn._next
 
 
-"""Array merge sort
-
-def merge(A,p,q,r):  
+def merge_a(A,p,q,r):  
     n1 = q - p + 1
     n2 = r - q
    # print("\nmerging, p="+str(p)+' q='+str(q)+" r="+str(r)+" n1="+str(n1)+" n2="+str(n2) + ", arr[p:r]="+str(arr[p:r+1]))
@@ -202,5 +151,29 @@ def mergesort(A,p,r):
         mergesort(A,p,q)
        # print("calling merge_sort(right half) with p="+str(q+1)+' r='+str(r))
         mergesort(A,q+1,r)
-        merge(A,p,q,r)
-"""
+        merge_a(A,p,q,r)
+        
+ 
+if __name__ == '__main__':
+    
+    times = []
+    times2 = []
+ 
+    for i in range(10,1000,10):
+        keys = np.random.randint(0, 1000, i)
+        d = _DoublyLinkedBase()
+        for key in keys:
+            d._insert_between(key, d._header, d._header._next)
+        start_t = time.time()
+        head = d_mergesort(d._header)
+        times.append(time.time() - start_t)
+        #plt.plot(times)
+        start_t = time.time()
+        mergesort(keys,0,i-1)
+        times2.append(time.time() - start_t)
+    
+    #x_ticks = range(10,2600,200)
+    #plt.xticks(x_ticks)
+    plt.plot(times, label="doubly-linked list")
+    plt.plot(times2, label="array")
+    plt.legend()
