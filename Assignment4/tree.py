2432f27e2c66781987d9f4879d30bc6dc22eb691
lemonci <hongyanchangfei@gmail.com>
2022-03-16 17:18:03 -0400

    add linkedtree class

--- Assignment4/tree.py
+++ Assignment4/tree.py
@@ -97,4 +97,89 @@ class GeneralTree(Tree):
                         
 class LinkedTree(GeneralTree):
     class _Node:    # Lightweight, nonpublic clss for stroing a node.
-        __slots__ = 'element', '_parent', 'c_list'
+        __slots__ = 'element', '_parent', '_c_list'
+        def __init__(self, element, parent = None):
+            self._element = element
+            self._parent = parent
+            self._c_list = []
+            
+        class Position(GeneralTree.Position):
+            """An abstraction representing the location of a single element."""
+            
+            def __init__(self, container, node):
+                """Constructor should not be invoked by user."""
+                self._container = container
+                self._node = node
+                
+            def element(self):
+                """Return the element stored at this Position"""
+                return self._node._element
+            
+            def __eq__(self, other):
+                """Return True if other is a Position representing the same location."""
+                return type(other) is type(self) and other._node is self._node
+            
+            def _validate(self, p):
+                """Return associate node, if position is valide."""
+                if not isinstance(p, self.Position):
+                    raise TypeError('p must be proper Position type')
+                if p._container is not self:
+                    raise ValueError('p does not belong to this container')
+                if p._node._parent is p._node: # convention for depreciated nodes
+                    raise ValueError('p is no longer valid')
+                return p._node
+            
+            def _make_position(self, node):
+                """Return Position instance for given node (or None if no node)."""
+                return self.Position(self, node) if node is not None else None
+            
+    #-------------------------- tree constructor --------------------------
+    def __init__(self):
+        """Create an initially empty tree."""
+        self._root = None
+        self._size = 0
+        
+    #-------------------------- public accessors --------------------------
+    def __len__(self):
+        """Return the total number of elements in the tree."""
+        return self._size
+    
+    def root(self):
+        """Return the root Position of the tree (or None if tree is empty)."""
+        return self._make_position(node._root)
+    
+    def parent(self, p):
+        """Return the Position of p's parent (or None if p is root)."""
+        node = self._validate(p)
+        return self._make_position(node._right)
+    
+    def visit_child(self, p, n):
+        """Return the Position of p's nth child (or None if no child)."""
+        node = self._validate(p)
+        return self._make_position(node._c_list[n])
+    
+    def num_children(self, p):
+        """Return the number of children of Position p."""
+        node = self._validate(p)
+        return len(node._c_list)
+    
+    def _add_root(self, e):
+        """Place element e at the root of an empty tree and return new Position.
+        
+        Raise ValueError if tree nonempty.
+        """
+        if self._root is not None: raise ValueError('Root exists')
+        self._size = 1
+        self._root = self._Node(e)
+        return self._make_position(self._root)
+    
+    def _add_child(self, p, e):
+        """Create a new left child for Position p, storing element e.
+        
+        Return the Position of new node.
+        Raise ValueError if Position p is invalide or p already has a left child.
+        """
+        node = self._validate(p)
+        self._size += 1
+        node._c_list.append(self._Node(e, node))
+        return self._make_position(node._c_list[-1])
