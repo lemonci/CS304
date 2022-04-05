d1e7d37272603f52f1235335899dbd33b8eb3cca
lemonci <hongyanchangfei@gmail.com>
2022-04-05 17:39:32 -0400

    hash table

--- Assignment5/map.py
+++ Assignment5/map.py
@@ -103,18 +103,57 @@ class BST:
 class MapBase(MutableMapping):
     """Our own abstract base class that includes a nonpublic Item class."""
 #------------------------------- nested Item class -------------------------------
-    class Item:
+    class _Item:
     """Lightweight composite to store key-value pairs as map items."""
-        slots = _key , _value
-        def init (self, k, v):
-            self. key = k
-            self. value = v       
-        def eq (self, other):
-            return self. key == other. key # compare items based on their keys
-        def ne (self, other):
-            return not (self == other) # opposite of eq
-        def lt (self, other):
-            return self. key < other. key # compare items based on their keys
+        __slots__ = '_key', '_value'
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v       
+        def __eq__(self, other):
+            return self._key == other._key # compare items based on their keys
+        def __ne__(self, other):
+            return not (self == other) # opposite of __eq__
+        def __lt__(self, other):
+            return self._key < other._key # compare items based on their keys
+
+class UnsortedTableMap(MapBase):
+    """Map implementation using an unordered list."""
+    def __init__(self):
+        """Create an empty map."""
+        self._table = []
+        
+    def __getitem__(self, k):
+        """Return value associated with key k (raise KeyError if not found)."""
+        for item in self._table:
+            if k == item._key:
+                return item._value
+        raise KeyError('Key Error: ' + repr(k))
+
+    def __setitem__(self, k, v):
+        """Assign value v to key k, overwriting existing value if present."""
+        for item in self._table:
+            if k == item._key:                  # Found a match:
+                item._value = v                 # reassign value
+                return                          # and quit
+           # did not find match for key
+        self._table.append(self._Item(k,v))
+        
+    def __delitem__(self, k):
+        """Remove item associated with key k (raise KeyError if not found)."""
+        for j in range(len(self._table)):
+            if k == self._table[j]._key:       # Found a match
+                self._table.pop(j)             # remove item
+                return                         # and quit
+        raise KeyError('Key Error: ' + repr(k))
+        
+    def __len__(self):
+        """Return number of items in the map."""
+        return len(self._table)
+        
+    def __iter__(self):
+        """Generate iteration of the map's keys."""
+        for item in self._table:
+            yield item._key                   # yield the KEY
 
 class HashMapBase(MapBase):
     """Abstract base class for map using hash-table with MAD compression."""
@@ -132,7 +171,7 @@ class HashMapBase(MapBase):
     def __len__(self):
         return self._n
     
-    def __getiem__(self, k):
+    def __getitem__(self, k):
         j = self._hash_function(k)
         return self._bucket_getitem(j, k)   # may raise KeyError
 
@@ -143,17 +182,97 @@ class HashMapBase(MapBase):
             self._resize(2 * len(self._table)-1)    # number 2^x - 1 is often prime
             
     def __delitem__(self, k):
-29 j = self. hash function(k)
-30 self. bucket delitem(j, k) # may raise KeyError
-31 self. n −= 1
-32
-33 def resize(self, c): # resize bucket array to capacity c
-34 old = list(self.items( )) # use iteration to record existing items
-35 self. table = c [None] # then reset table to desired capacity
-36 self. n = 0 # n recomputed during subsequent adds
-37 for (k,v) in old:
-38 self[k] = v # reinsert old key-value pair
+        j = self._hash_function(k)
+        self._bucket_delitem(j, k) # may raise KeyError
+        self._n −= 1
+        
+    def resize(self, c): # resize bucket array to capacity c
+        old = list(self.items( )) # use iteration to record existing items
+        self._table = c [None] # then reset table to desired capacity
+        self._n = 0 # n recomputed during subsequent adds
+        for (k,v) in old:
+            self[k] = v # reinsert old key-value pair
+
+# 2) Hash table (separate chaining)
+class ChainHashMap(HashMapBase):
+    # Hash map implemented with separate chaining for collision resolution.
+    def _bucket_getitem(self, j, k):
+        bucket = self. table[j]
+        if bucket is None:
+            raise KeyError( 'Key Error: '+ repr(k)) # no match found
+        return bucket[k] # may raise KeyError
+        
+    def _bucket_setitem(self, j, k, v):
+        if self._table[j] is None:
+            self._table[j] = UnsortedTableMap( ) # bucket is new to the table
+        oldsize = len(self. table[j])
+        self._table[j][k] = v
+        if len(self._table[j]) > oldsize: # key was new to the table
+            self. n += 1 # increase overall map size
+
+    def _bucket_delitem(self, j, k):
+        bucket = self. table[j]
+        if bucket is None:
+            raise KeyError('Key Error: '+ repr(k)) # no match found
+        del bucket[k] # may raise KeyError
+
+    def __iter__(self):
+        for bucket in self._table:
+            if bucket is not None: # a nonempty slot
+                for key in bucket:
+                    yield key
+
+# 3) Hash table (linear probing)
+class ProbeHashMap(HashMapBase):
+    """Hash map implemented with linear probing for collision resolution."""
+    _AVAIL = object()   # sentinal marks locations of previous deletions
     
+    def _is_available(self, j):
+        """Return True if index j is available in table."""
+        return self._table[j] is None or self._table[j] is ProbeHashMap._AVAIL
+    
+    def _find_slot(self, j, k):
+        """Search for key k in bucket at index j.
+        
+        Return (success, index) tuple, described as follows:
+        if match was found, success is True and index denotes its location.
+        if no match found, success is False and index denotes first available slot.
+        """
+        firstAvail = None
+        while True:
+            if self._is_available(j):
+                if firstAvail is None:
+                    firstAvail = j          # mark this as first available
+                if self._table[j] is None:
+                    return (False, firstAvail) # search has failed
+            elif k == self._table[j]._key:
+                return (True, j)                # found a match
+            j = ( j + 1 ) % len (self._table)   # keep looking (cyclically)
+            
+    def _bucket_getitem(self, j, k):
+        found, s = self._find_slot(j, k)
+        if not found:
+            raise KeyError('Key Error: ' + repr(k)) # no match found
+        return self._table[s]._value
+        
+    def _bucket_setitem(self, j, km v):
+        found, s = self._find_slot(j, k)
+        if not found:
+            self._table[s] = self._Item(k, v)   # insert new item
+            self._n += 1                        # size has increased
+        else:
+            self._table[s]._vale = v            # overwrite existing
+            
+    def _bucket_delitem(self, j, k):
+        found, s = self._find_slot(j, k)
+        if not found:
+            raise KeyError('Key Error: ' + repr(k)) # no match found
+        self._table[s] = ProbeHashMap._AVAIL        # mark as vacated
+
+    def __iter__(self):     # scan entire table
+        for j in range(len(self._table)):
+            if not self._is_available(j):
+                yield self._table[j]._key
 LEN = 10
 keys = list(range(LEN))
 shuffle(keys)
