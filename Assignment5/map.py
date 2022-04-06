e98aed09a40fa3b5f4d534440a3bc74a2512f953
lemonci <hongyanchangfei@gmail.com>
2022-04-06 19:31:59 -0400

    skip list & report

--- Assignment5/map.py
+++ Assignment5/map.py
@@ -1,9 +1,8 @@
-import random
-from random import shuffle, randrange
+import math, os, time
+from random import shuffle, randrange, random, Random
 from collections.abc import MutableMapping
-import time
 from matplotlib import pyplot as plt
-
+import numpy as np
 
 #1) BST (binary search tree)
 class BST:
@@ -17,7 +16,7 @@ class BST:
             self.pair = pair
             return
         if self.pair[0] == pair[0]:
-            return
+            raise KeyError("The key has already in the tree.")
         if pair[0] < self.pair[0]:
             if self.left:
                 self.left.insert(pair)
@@ -91,19 +90,21 @@ class BST:
         
     def __setitem__(self, key, val):
         pair_n = (key, val)
-        if key == self.pair[0]:
-            self.pair = pair_n
-            return
-        if key < self.pair[0]:
-            if self.left == None:
+        try:
+            self.insert(pair_n)
+        except:
+            if key == self.pair[0]:
+                self.pair = pair_n
+                return
+            if key < self.pair[0]:
+                if self.left.pair == None:
+                    raise KeyError("%s is not in the tree." % key)
+                self.left.__setitem__(key, val)
+            if self.right.pair == None:
                 raise KeyError("%s is not in the tree." % key)
-            self.left.__setitem__(key, val)
-        if self.right == None:
-            raise KeyError("%s is not in the tree." % key)
-        self.right.__setitem__(key, val)
+            self.right.__setitem__(key, val)
 
 # HashMapBase
-
 
 class MapBase(MutableMapping):
     """Our own abstract base class that includes a nonpublic Item class."""
@@ -278,8 +279,308 @@ class ProbeHashMap(HashMapBase):
         for j in range(len(self._table)):
             if not self._is_available(j):
                 yield self._table[j]._key
-                
-                
+
+# 4) Skip list
+class SkipList(object):
+    """An indexable skip list.
+    A SkipList provides an ordered sequence of key-value pairs. The list is
+    always sorted on key and supports O(1) forward iteration. It has O(log N)
+    time complexity for key lookup, pair insertion and pair removal anywhere in
+    the list. The list also supports O(log N) element access by position.
+    The keys of all pairs you add to the skiplist must be be comparable against
+    each other, and define the ``<`` and ``<=`` operators.
+    """
+
+    UNSET = object()
+
+    p = int((1<<31) / math.e)
+    maxlevel = 20
+
+    _rnd = Random()
+    _rnd.seed(os.urandom(16))
+
+    __slots__ = ('_level', '_head', '_tail', '_path', '_distance')
+
+    def __init__(self):
+        self._level = 1
+        self._head = self._new_node(self.maxlevel, None, None)
+        self._tail = self._new_node(self.maxlevel, None, None)
+        for i in range(self.maxlevel):
+            self._head[2+i] = self._tail
+        self._path = [None] * self.maxlevel
+        self._distance = [None] * self.maxlevel
+
+    def _new_node(self, level, key, value):
+        # Node layout: [key, value, next*LEVEL, skip?]
+        # The "skip" element indicates how many nodes are skipped by the
+        # highest level incoming link.
+        if level == 1:
+            return [key, value, None]
+        else:
+            return [key, value] + [None]*level + [0]
+
+    def _random_level(self):
+        # Exponential distribution as per Pugh's paper.
+        l = 1
+        maxlevel = min(self.maxlevel, self.level+1)
+        while l < maxlevel and self._rnd.getrandbits(31) < self.p:
+            l += 1
+        return l
+
+    def _create_node(self, key, value):
+        # Create a new node, updating the list level if required.
+        level = self._random_level()
+        if level > self.level:
+            self._tail[-1] = len(self)
+            self._level = level
+            self._path[level-1] = self._head
+            self._distance[level-1] = 0
+        return self._new_node(level, key, value)
+
+    def _find_lt(self, key):
+        # Find path to last node < key
+        node = self._head
+        distance = 0
+        for i in reversed(range(self.level)):
+            nnode = node[2+i]
+            while nnode is not self._tail and nnode[0] < key:
+                nnode, node = nnode[2+i], nnode
+                distance += 1 if i == 0 else node[-1]
+            self._path[i] = node
+            self._distance[i] = distance
+
+    def _find_lte(self, key):
+        # Find path to last node <= key
+        node = self._head
+        distance = 0
+        for i in reversed(range(self.level)):
+            nnode = node[2+i]
+            while nnode is not self._tail and nnode[0] <= key:
+                nnode, node = nnode[2+i], nnode
+                distance += 1 if i == 0 else node[-1]
+            self._path[i] = node
+            self._distance[i] = distance
+
+    def _find_pos(self, pos):
+        # Create path to node at pos.
+        node = self._head
+        distance = 0
+        for i in reversed(range(self.level)):
+            nnode = node[2+i]
+            ndistance = distance + (1 if i == 0 else nnode[-1])
+            while nnode is not self._tail and ndistance <= pos:
+                nnode, node, distance = nnode[2+i], nnode, ndistance
+                ndistance += 1 if i == 0 else nnode[-1]
+            self._path[i] = node
+            self._distance[i] = distance
+
+    def _insert(self, node):
+        # Insert a node in the list. The _path and _distance must be set.
+        path, distance = self._path, self._distance
+        # Update pointers
+        level = max(1, len(node) - 3)
+        for i in range(level):
+            node[2+i] = path[i][2+i]
+            path[i][2+i] = node
+        if level > 1:
+            node[-1] = 1 + distance[0] - distance[level-1]
+        # Update skip counts
+        node = node[2]
+        i = 2; j = min(len(node) - 3, self.level)
+        while i <= self.level:
+            while j < i:
+                node = node[i]
+                j = min(len(node) - 3, self.level)
+            node[-1] -= distance[0] - distance[j-1] if j <= level else -1
+            i = j+1
+
+    def _remove(self, node):
+        # Remove a node. The _path and _distance must be set.
+        path, distance = self._path, self._distance
+        level = max(1, len(node) - 3)
+        for i in range(level):
+            path[i][2+i] = node[2+i]
+        # Update skip counts
+        value = node[1]
+        node = node[2]
+        i = 2; j = min(len(node) - 3, self.level)
+        while i <= self.level:
+            while j < i:
+                node = node[i]
+                j = min(len(node) - 3, self.level)
+            node[-1] += distance[0] - distance[j-1] if j <= level else -1
+            i = j+1
+        # Reduce level if last node on current level was removed
+        while self.level > 1 and self._head[1+self.level] is self._tail:
+            self._level -= 1
+            self._tail[-1] += self._tail[-1] - len(self)
+        return value
+
+    # PUBLIC API ...
+
+    @property
+    def level(self):
+        """The current level of the skip list."""
+        return self._level
+
+    def insert(self, key, value):
+        """Insert a key-value pair in the list.
+        The pair is inserted at the correct location so that the list remains
+        sorted on *key*. If a pair with the same key is already in the list,
+        then the pair is appended after all other pairs with that key.
+        """
+        self._find_lte(key)
+        node = self._create_node(key, value)
+        self._insert(node)
+
+    def __setitem__(self, key, value):
+        """Replace the value of the first key-value pair with key *key*.
+        If the key was not found, the pair is inserted.
+        """
+        self._find_lt(key)
+        node = self._path[0][2]
+        if node is self._tail or key < node[0]:
+            node = self._create_node(key, value)
+            self._insert(node)
+        else:
+            node[1] = value
+
+    def clear(self):
+        """Remove all key-value pairs."""
+        for i in range(self.maxlevel):
+            self._head[2+i] = self._tail
+            self._tail[-1] = 0
+        self._level = 1
+
+    def __len__(self):
+        """Return the number of pairs in the list."""
+        dist = 0
+        idx = self.level + 1
+        node = self._head[idx]
+        while node is not self._tail:
+            dist += node[-1] if idx > 2 else 1
+            node = node[idx]
+        dist += node[-1]
+        return dist
+
+    __bool__ = __nonzero__ = lambda self: len(self) > 0
+
+    def __repr__(self):
+        return type(self).__name__ + '((' + repr(list(self.items()))[1:-1] + '))'
+
+    def items(self, start=None, stop=None):
+        """Return an iterator yielding pairs.
+        If *start* is specified, iteration starts at the first pair with a key
+        that is larger than or equal to *start*. If not specified, iteration
+        starts at the first pair in the list.
+        If *stop* is specified, iteration stops at the last pair that is
+        smaller than *stop*. If not specified, iteration end with the last pair
+        in the list.
+        """
+        if start is None:
+            node = self._head[2]
+        else:
+            self._find_lt(start)
+            node = self._path[0][2]
+        while node is not self._tail and (stop is None or node[0] < stop):
+            yield (node[0], node[1])
+            node = node[2]
+
+    __iter__ = items
+
+    def keys(self, start=None, stop=None):
+        """Like :meth:`items` but returns only the keys."""
+        return (item[0] for item in self.items(start, stop))
+
+    def values(self, start=None, stop=None):
+        """Like :meth:`items` but returns only the values."""
+        return (item[1] for item in self.items(start, stop))
+
+    def popitem(self):
+        """Removes the first key-value pair and return it.
+        This method raises a ``KeyError`` if the list is empty.
+        """
+        node = self._head[2]
+        if node is self._tail:
+            raise KeyError('list is empty')
+        self._find_lt(node[0])
+        self._remove(node)
+        return (node[0], node[1])
+
+    # BY KEY API ...
+
+    def __getitem__(self, key, default=None):
+        """Find the first key-value pair with key *key* and return its value.
+        If the key was not found, return *default*. If no default was provided,
+        return ``None``. This method never raises a ``KeyError``.
+        """
+        self._find_lt(key)
+        node = self._path[0][2]
+        if node is self._tail or key < node[0]:
+            return default
+        return node[1]
+
+    def __delitem__(self, key):
+        """Remove the first key-value pair with key *key*.
+        If the key was not found, a ``KeyError`` is raised.
+        """
+        self._find_lt(key)
+        node = self._path[0][2]
+        if node is self._tail or key < node[0]:
+            raise KeyError('{!r} is not in list'.format(key))
+        self._remove(node)
+
+    def pop(self, key, default=UNSET):
+        """Remove the first key-value pair with key *key*.
+        If a pair was removed, return its value. Otherwise if *default* was
+        provided, return *default*. Otherwise a ``KeyError`` is raised.
+        """
+        self._find_lt(key)
+        node = self._path[0][2]
+        if node is self._tail or key < node[0]:
+            if default is self.UNSET:
+                raise KeyError('key {!r} not in list')
+            return default
+        self._remove(node)
+        return node[1]
+
+    def __contains__(self, key):
+        """Return whether *key* is contained in the list."""
+        self._find_lt(key)
+        node = self._path[0][2]
+        return node is not self._tail and not key < node[0]
+
+    def index(self, key, default=UNSET):
+        """Find the first key-value pair with key *key* and return its position.
+        If the key is not found, return *default*. If default was not provided,
+        raise a ``KeyError``
+        """
+        self._find_lt(key)
+        node = self._path[0][2]
+        if node is self._tail or key < node[0]:
+            if default is self.UNSET:
+                raise KeyError('key {!r} not in list'.format(key))
+            return default
+        return self._distance[0]
+
+    def count(self, key):
+        """Return the number of pairs with key *key*."""
+        count = 0
+        pos = self.index(key, -1)
+        if pos == -1:
+            return count
+        count += 1
+        for i in range(pos+1, len(self)):
+            if self[i][0] != key:
+                break
+            count += 1
+        return count        
+
+tree = BST()
+c = ChainHashMap()
+p = ProbeHashMap()
+s = SkipList()
+
 LEN = 10000
 keys = list(range(LEN))
 shuffle(keys)
@@ -290,18 +591,14 @@ t_s = [None] * LEN
 
 dic = []
 for key in keys:
-    dic.append((key, random.random()))
+    dic.append((key, random()))
 
-
-tree = BST()
-c = ChainHashMap()
-p = ProbeHashMap()
 # ============================== Insert ==============================
 
 for i in range(LEN):
 # ============================== BST =================================
     t = time.process_time()
-    tree.insert(dic[i])
+    tree[dic[i][0]] = dic[i][1]
     t = time.process_time() - t
     t_tree[i] = t
 # =========================== Hash Table (Chain)======================
@@ -314,15 +611,20 @@ for i in range(LEN):
     p[dic[i][0]] = dic[i][1]
     t = time.process_time() - t
     t_p[i] = t
+# =========================== Skip List ==============================
+    t = time.process_time()
+    s[dic[i][0]] = dic[i][1]
+    t = time.process_time() - t
+    t_s[i] = t
 # ====================================================================
 
 
-# =========================== Insert & Visit =========================
+# # =========================== Insert & Visit =========================
 
 for i in range(LEN):
 # ============================== BST =================================
     t = time.process_time()
-    tree.insert(dic[i])
+    tree[dic[i][0]] = dic[i][1]
     v = tree[dic[i][0]]
     t = time.process_time() - t
     t_tree[i] = t
@@ -338,31 +640,50 @@ for i in range(LEN):
     v = p[dic[i][0]]
     t = time.process_time() - t
     t_p[i] = t
+# =========================== Skip List ==============================
+    t = time.process_time()
+    s[dic[i][0]] = dic[i][1]
+    v = s[dic[i][0]]
+    t = time.process_time() - t
+    t_s[i] = t
 # ====================================================================
 
-# ============================== Delete ==============================
-
-for i in range(LEN):
+# # ============================== Delete ==============================
+a = list(range(LEN))
+shuffle(a)
+for i in a:
 # ============================== BST =================================
     t = time.process_time()
-    del tree[dic[i][0]]
+    del tree[i]
     t = time.process_time() - t
     t_tree[i] = t
 # =========================== Hash Table (Chain)======================
     t = time.process_time()
-    del c[dic[i][0]]
+    del c[i]
     t = time.process_time() - t
     t_c[i] = t
 # =========================== Hash Table (Chain)======================
     t = time.process_time()
-    del p[dic[i][0]]
+    del p[i]
     t = time.process_time() - t
     t_p[i] = t
+# =========================== Skip List ==============================
+    t = time.process_time()
+    del s[i]
+    t = time.process_time() - t
+    t_s[i] = t
 # ====================================================================
 
 
 # ===========================plot==============================================
-plt.plot(t_tree, 'r')
-plt.plot(t_c, 'b')
-plt.plot(t_p, 'g')
-# plt.plot(t_s, 'm')
+
+t_tree_10 = list(np.add.reduceat(t_tree, np.arange(0, LEN, 10)))
+t_c_10 = list(np.add.reduceat(t_c, np.arange(0, LEN, 10)))
+t_p_10 = list(np.add.reduceat(t_p, np.arange(0, LEN, 10)))
+t_s_10 = list(np.add.reduceat(t_s, np.arange(0, LEN, 10)))
+
+plt.plot(t_tree_10, 'r', label="BST")
+plt.plot(t_c_10, 'b', label = "Hash-chain")
+plt.plot(t_p_10, 'g', label = "Hash-probing")
+plt.plot(t_s_10, 'm', label = "Skip list")
+plt.legend()
